7959465ec88c5f5acbc6ec7fda3f78eb07a52e25
Celal Yıldırım <celallee12@gmail.com>
2024-03-10 22:36:01 +0300

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace WinFormAppCalculator
+{
+	public partial class Calculator : Form
+	{
+		public Calculator()
+		{
+			InitializeComponent();
+		}
+
+		private void button0_Click(object sender, EventArgs e)
+		{
+
+		}
+		
+		private void button3_Click(object sender, EventArgs e)
+		{
+			
+
+		}
+
+		private void btn_CE_Click(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void btn_C_Click(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void btn_Equal_Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+
+
+	}
+}
